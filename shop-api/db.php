c72c376530015f977644de3cfd704fb60fb94e8b
ta1max <artjomsjasinskis@gmail.com>
2025-12-11 13:26:40 +0300

    поправил логин/пароль для MySQL

--- shop-api/db.php
+++ shop-api/db.php
@@ -1,18 +1,17 @@
 <?php
 $host = 'localhost';
-$dbname = 'shopdb'; // Имя БД, которую они создали
-$user = 'xxxx'; // имя пользователя 
-$pass = 'yyyy'; // пароль
+$dbname = 'shopdb';
+$user = 'root'; // ← по умолчанию
+$pass = '';     // ← пустой пароль
+
 $options = [
-PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES => false,
 ];
+
 try {
-$pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass,
-$options);
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass, $options);
 } catch (\PDOException $e) {
-// В реальном проекте здесь не стоит выводить ошибку, а писать в лог
-throw new \PDOException($e->getMessage(), (int)$e->getCode());
+    throw new \PDOException($e->getMessage(), (int)$e->getCode());
 }
-?>
